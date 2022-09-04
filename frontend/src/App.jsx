13e740612e0200c5cee95325b2b7af5884aae053
Paras Gupta <guptaparas039@gmail.com>
2022-09-04 20:36:33 +0000

    define layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ReadingAssessment from "./pages/ReadingAssessment";
+import ReadingData from "./data/reading/1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">ECS Module</header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reading-assessment" element={<ReadingAssessment />} />
+        <Route
+          path="/data"
+          element={<pre>{JSON.stringify(ReadingData, undefined, 2)}</pre>}
+        />
+        <Route path="/*" element={<h1>404 Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
