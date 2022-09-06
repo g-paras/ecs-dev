a207b379a23673ee9ee963b7202690895696fd63
Paras Gupta <guptaparas039@gmail.com>
2022-09-06 19:22:26 +0000

    create model for paragraph, question, option & make migrations

--- backend/reading/admin.py
+++ backend/reading/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Passage, Question, Option
+
 # Register your models here.
+admin.site.register(Passage)
+admin.site.register(Question)
+admin.site.register(Option)
--- backend/reading/models.py
+++ backend/reading/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Passage(models.Model):
+    content = models.TextField(_("passage"))
+
+
+class Question(models.Model):
+    passage_id = models.ForeignKey(
+        Passage, on_delete=models.CASCADE, related_name="questions"
+    )
+    question = models.CharField(_("passage question"), max_length=250)
+
+
+class Option(models.Model):
+    question_id = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="options"
+    )
+    value = models.CharField(_("question option"), max_length=100)
+    is_answer = models.BooleanField(_("is answer"), default=False)
