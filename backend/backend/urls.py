5084b8e45d9282729f05afa5e225ffce73f7cf99
Paras Gupta <guptaparas039@gmail.com>
2022-09-09 10:05:39 +0000

    add writing urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path("reading/", include("reading.urls")),
+    path("writing/", include("writing.urls")),
 ]
