e8ea5d8082dad3febc05a48a166dcd9ebef252b0
Paras Gupta <guptaparas039@gmail.com>
2022-09-27 16:20:31 +0000

    login method

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -8,13 +8,17 @@ export const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [token, setToken] = useState(null);
 
+  const login = () => {
+    setLoggedIn(true);
+  };
+
   useEffect(() => {
     let accessToken = localStorage.getItem("access_token_iuaov7w34");
     setToken(accessToken);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ loggedIn, token }}>
+    <AuthContext.Provider value={{ loggedIn, token, login }}>
       {children}
     </AuthContext.Provider>
   );
